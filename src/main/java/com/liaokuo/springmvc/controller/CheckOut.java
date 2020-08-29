b7e91bf7e636d7fd7ca9ddea9c6861841e088f81
mxoop <1592013653@qq.com>
2020-08-29 19:50:03 +0800

    2

--- src/main/java/com/liaokuo/springmvc/controller/CheckOut.java
+++ src/main/java/com/liaokuo/springmvc/controller/CheckOut.java
@@ -1,7 +1,10 @@
 package com.liaokuo.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Rain on 2020/8/29.
  */
+@Controller
 public class CheckOut {
 }
