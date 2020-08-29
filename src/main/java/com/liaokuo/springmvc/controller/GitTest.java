68a5e30f1aaed5af77459118c2466e7db2dc8e15
mxoop <62545067+mxoop@users.noreply.github.com>
2020-08-29 19:56:40 +0800

    3

--- src/main/java/com/liaokuo/springmvc/controller/GitTest.java
+++ src/main/java/com/liaokuo/springmvc/controller/GitTest.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GitTest {
     @RequestMapping
     public String test(){
-        Dept dept = new Dept();
         return  null;
     }
 }
