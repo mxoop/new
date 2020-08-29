640b1c4283207c6b62c1ec969ba35c7eda4da98d
mxoop <1592013653@qq.com>
2020-08-29 18:47:07 +0800

    test1

---
+++ src/main/java/com/liaokuo/springmvc/controller/GitTest.java
@@ -0,0 +1,15 @@
+package com.liaokuo.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rain on 2020/8/29.
+ */
+@Controller
+public class GitTest {
+    @RequestMapping
+    public String test(){
+        return  null;
+    }
+}
